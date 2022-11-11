72760f3521f63a8bf289d6b853d8d58f0f7a01d9
happydragon227 <114888200+happydragon227@users.noreply.github.com>
2022-11-10 21:53:22 -0500

    Add files via upload

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,19 @@
 // TODO: create a component that displays a single bakery item
+import "../App.css";
+import App from "../App";
+import { useState } from "react";
+import "./BakeryItem.css";
+
+function BakeryItem({item, addCounter}) {
+    return (
+        <div class="bakeItem">
+            <img src={item.image} width="30%"/>
+            <h3>{item.name}</h3>
+            <p>{item.description}</p>
+            <p>${item.price}</p>
+            <button onClick={() => addCounter(item.price,item.name)}> Add </button>
+        </div> 
+    );
+}
+
+export default BakeryItem;
